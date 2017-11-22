ecd6f9be26edeab30cc1d88dd5d15c1089d3b31f
Jay Williams <course@jaywilliams.me>
2017-11-22 18:25:01 +0000

    FIXED FEELSEDDIEMAN COMMAND

--- src/commands/util/feelseddieman.js
+++ src/commands/util/feelseddieman.js
@@ -9,8 +9,8 @@ class PingCommand extends Command {
     }
 
     async exec(message) {
-        var m = message.channel.messages.first(2)[0];
-        console.log(m)
+        var ms = await message.channel.messages.fetch( {limit: 2} );
+        var m = Array.from(ms)[1][1];
         var eddies = ["382942158682062850", "382942191288320002", "382942228840185856", "382942312654962699", "382942381382828033", "382942385103044608", "382942387858833408", "382942391620861953", "382942393433063436", "382942482792710164"];
         eddies.map( async (eddie) => {
             await m.react(eddie);
