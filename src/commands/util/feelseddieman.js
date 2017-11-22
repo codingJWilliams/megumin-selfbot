a9f32e1aad844e0394e094237f204861c3dfa448
Jay Williams <codingJWilliams@users.noreply.github.com>
2017-11-22 17:40:49 +0000

    Update feelseddieman.js

--- src/commands/util/feelseddieman.js
+++ src/commands/util/feelseddieman.js
@@ -11,7 +11,7 @@ class PingCommand extends Command {
     async exec(message) {
         var chan = message.channel;
         await message.delete();
-        var m = await message.channel.fetchMessages({ limit: 1 });
+        var m = await chan.fetchMessages({ limit: 1 });
         m.map(async (m) => {
           var eddies = ["382942158682062850", "382942191288320002", "382942228840185856", "382942312654962699", "382942381382828033", "382942385103044608", "382942387858833408", "382942391620861953", "382942393433063436", "382942482792710164"];
           eddies.map( async (eddie) => {
