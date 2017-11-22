d5a739d64dc144ceabda7bb1d48033effbc8a3b0
Jay Williams <course@jaywilliams.me>
2017-11-22 18:14:41 +0000

    Bit of progress

--- src/commands/util/feelseddieman.js
+++ src/commands/util/feelseddieman.js
@@ -9,17 +9,13 @@ class PingCommand extends Command {
     }
 
     async exec(message) {
-        console.log(message.channel)
-        var m = await message.channel.fetchMessages({limit: 10})
-        message.reply(m);
-        await message.delete();
-        
-        /*m.map(async (m) => {
-          var eddies = ["382942158682062850", "382942191288320002", "382942228840185856", "382942312654962699", "382942381382828033", "382942385103044608", "382942387858833408", "382942391620861953", "382942393433063436", "382942482792710164"];
-          eddies.map( async (eddie) => {
+        var m = message.channel.messages.first(2)[0];
+        console.log(m)
+        var eddies = ["382942158682062850", "382942191288320002", "382942228840185856", "382942312654962699", "382942381382828033", "382942385103044608", "382942387858833408", "382942391620861953", "382942393433063436", "382942482792710164"];
+        eddies.map( async (eddie) => {
             await m.react(eddie);
-          })
-        })*/
+        })
+        await message.delete();
     }
 }
 
